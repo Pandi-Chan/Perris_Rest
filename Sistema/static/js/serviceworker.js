c06dd792c3dbdc42528164d9a75df33d9153d637
John Molina Becker <42702401+Pandi-Chan@users.noreply.github.com>
2018-11-27 13:14:23 -0300

    Worker Arreglado

--- Sistema/static/js/serviceworker.js
+++ Sistema/static/js/serviceworker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/','/index/'
+        '/base_layout'
       ]);
     })
   );
@@ -12,9 +12,9 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin) {
-      if ((requestUrl.pathname === '/')) {
-        event.respondWith(caches.match('/','/index/'));
+    if (requestUrl.origin == location.origin) {
+      if ((requestUrl.pathname == '/')) {
+        event.respondWith(caches.match('/base_layout'));
         return;
       }
     }
